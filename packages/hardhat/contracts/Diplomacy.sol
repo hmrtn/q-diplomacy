0d83485c4fbd386b646888a1b379f53be3fd2fb5
lazlo <lazl0x@pm.me>
2021-08-17 08:00:17 -0700

    updated contract

--- packages/hardhat/contracts/Diplomacy.sol
+++ packages/hardhat/contracts/Diplomacy.sol
@@ -9,11 +9,18 @@ contract Diplomacy {
 
     using SafeMath for uint;
 
+    struct Ballot {
+        uint castAt;
+    }
+
     struct Election {
         string name; 
+        bool active;
         address[] candidates;
-        mapping (address => uint) score; 
+        mapping (address => uint) score;
+        mapping (address => uint) result; 
         mapping (address => bool) voted;
+        mapping (address => Ballot) ballot;
     }
 
     mapping (uint => Election) elections; 
@@ -21,18 +28,82 @@ contract Diplomacy {
     uint numElections;
 
     event ElectionCreated(address _creator, uint _electionId);
+    event BallotCast(address _caster);
+    event ElectionEnded(uint _electionId);
     
     function newElection(
         string memory _name, 
         address[] memory _candidates
-    ) public returns (uint electionId) {
+    ) public payable returns (uint electionId) {
         
-        electionId = numElections++; 
+        electionId = numElections.add(1); 
         Election storage election = elections[electionId];
         election.name = _name; 
         election.candidates = _candidates; 
+        election.active = true;
 
         emit ElectionCreated(msg.sender, electionId);
+
+    }
+
+    function endElection(uint electionId) public {
+
+        Election storage election = elections[electionId];
+        election.active = false; 
+
+        for ( uint i = 0; i < election.candidates.length; i++ ) {
+            address candidate = election.candidates[i];
+            election.result[candidate] = election.result[candidate] * election.result[candidate];
+        }
+        
+        _payout(electionId);
+
+        emit ElectionEnded(electionId);
+
+    }
+
+    function castBallot(
+        uint electionId,
+        address[] memory _adrs, 
+        uint[] memory _votes
+    ) public { 
+
+        Election storage election = elections[electionId];
+        
+        election.ballot[msg.sender] = Ballot({
+            castAt: block.timestamp
+        });
+
+        for ( uint i = 0; i < election.candidates.length; i++ ) {
+            election.score[_adrs[i]].add(sqrt(_votes[i]));
+        }
+
+        emit BallotCast(msg.sender);
+
+    }
+
+    function _payout(uint electionId) internal {
+
+        Election storage election = elections[electionId]; 
+        
+        uint total; 
+        for ( uint i = 0; i < election.candidates.length; i++) {
+            total += election.result[election.candidates[i]];
+        }
+
+        console.log(total);
+
+    }
+
+    // Helpers
+    function sqrt(uint x) internal pure returns (uint y) {
+
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
 
     }
 
